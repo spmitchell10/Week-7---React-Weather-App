bf5e4a4921e98b09e284906987fea2ceb3bd0667
Stephen Mitchell <stephenmitchell@Stephens-MacBook-Pro.local>
2017-04-13 10:05:33 -0400

    bootstrap fix

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
           })
       })
   } 
-
+// This is our default state. 
   getWeather(){
 
       axios.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${this.city.value}&mode=json&units=imperial&cnt=6&APPID=b5004642f125f0706b3f6d7616fa2635`)
@@ -46,19 +46,19 @@ class App extends Component {
           <h2>Weather.React</h2>
         </div>
             <form>
-              <div class="form-group">
+              <div className="form-group">
                 <label for="exampleInputEmail1">Enter City</label>
-                <input type="text" class="form-control" ref={(input) => {this.city = input}} placeholder="City"></input> 
+                <input type="text" className="form-control" ref={(input) => {this.city = input}} placeholder="City"></input> 
               </div>
-              <button type="submit" class="btn btn-default" onClick={this.getWeather.bind(this)}>Submit</button>
+              <button type="submit" className="btn btn-default" onClick={this.getWeather.bind(this)}>Submit</button>
             </form>
                     
         <div className="container-fluid App-intro">
 
-          {this.state.weather.map(day=>
+          {this.state.weather.map(day=> //this.state.weather is our array. "map" is just native JS. "map" is functional that returns back a new array
             <div className="col-md-4 clearfix">
-            <h2 className="temp">High: {day.temp.max}</h2>
-            <h2 className="temp">Low: {day.temp.min}</h2>
+            <h2 className="temp">High: {day.temp.max}℉</h2>
+            <h2 className="temp">Low: {day.temp.min}℉</h2>
             <h5 className="temp">{day.weather[0].description}</h5>
             </div>
             )}
